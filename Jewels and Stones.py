bb4066f784f00e96697244c27a42be35ab9ec94d
Zachary Manesiotis <zack.manesiotis@gmail.com>
2019-05-03 23:28:22 -0400

    leetcode easy

---
+++ Jewels and Stones.py
@@ -0,0 +1,19 @@
+# Jewels and Stones - leetcode easy 
+
+class Solution:
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        count = 0 
+        for i in range(len(J)): 
+            for j in range(len(S)): 
+                if J[i] == S[j]: 
+                    count += 1 
+                    
+        return count
+        
+        
+        
