40b99ab97a6c1ffbc240b2c828fe386fd39ca84c
Zachary Manesiotis <zack.manesiotis@gmail.com>
2019-10-23 00:58:46 -0400

    Create min_in_rotated_sorted_array.py

---
+++ min_in_rotated_sorted_array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        for i in range(len(nums)): 
+            if i != len(nums) and nums[i] < nums[i-1]: 
+                return nums[i]
+            elif len(nums) == 1: 
+                return nums[0]
+        
