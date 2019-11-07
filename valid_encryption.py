e3834b0beaa3013020782c67c43d9854cc81c02f
Zachary Manesiotis <zack.manesiotis@gmail.com>
2019-11-07 16:30:32 -0500

    updated to consider an edge case

--- valid_encryption.py
+++ valid_encryption.py
@@ -1,12 +1,15 @@
 from collections import defaultdict 
 def possible_encoding(original, encrypted): 
     char_map = defaultdict(list)
-    for curr, new in zip(original, encrypted): 
-        if char_map[curr]: 
-            if new in char_map[curr]: 
-                continue 
-            elif new not in char_map[curr]: 
-                return False 
-        else: 
-            char_map[curr] = new 
-    return True 
+    if len(original) == len(encrypted): 
+        for curr, new in zip(original, encrypted): 
+            if char_map[curr]: 
+                if new in char_map[curr]: 
+                    continue 
+                elif new not in char_map[curr]: 
+                    return False 
+            else: 
+                char_map[curr] = new 
+        return True 
+    else: 
+        return False
