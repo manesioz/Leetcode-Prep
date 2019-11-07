400cfb6c5222f9896df95762511bf1bf3b43c1d9
Zachary Manesiotis <zack.manesiotis@gmail.com>
2019-11-07 16:23:38 -0500

    valid string encryption - leetcode medium
    valid iff there exists a 1:1 mapping from each char in the old string to the new string

---
+++ valid_encryption.py
@@ -0,0 +1,12 @@
+from collections import defaultdict 
+def possible_encoding(original, encrypted): 
+    char_map = defaultdict(list)
+    for curr, new in zip(original, encrypted): 
+        if char_map[curr]: 
+            if new in char_map[curr]: 
+                continue 
+            elif new not in char_map[curr]: 
+                return False 
+        else: 
+            char_map[curr] = new 
+    return True 
