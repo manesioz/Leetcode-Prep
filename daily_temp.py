cb6b267ceb90b8f84448ea4a2b897dab3031bdb8
Zachary Manesiotis <zack.manesiotis@gmail.com>
2020-08-09 23:01:35 -0400

    fix: updated solution to daily temp - leetcode medium
    https://leetcode.com/problems/daily-temperatures/

--- daily_temp.py
+++ daily_temp.py
@@ -1,20 +1,12 @@
 class Solution:
-    # def dailyTemperatures(self, T: List[int]) -> List[int]: #sub-optimal, O(n^2)
-    #     output = [] 
-    #     for i, outer_num in enumerate(T): 
-    #         for j, inner_num in enumerate(T): 
-    #             if inner_num > outer_num and j > i: 
-    #                 output.append(j-i)
-    #                 break
-    #         if len(output) < len(T[:i+1]): 
-    #             output.append(0)
-    #     return output 
-    def dailyTemperatures(self, T: List[int]) -> List[int]: #solution using a stack, O(n) runtime 
-        stack = []
-        wait = [0]*len(T)
+    def dailyTemperatures(self, T: List[int]) -> List[int]:
+        # exact same solution of the "next greater element" problem 
+        # trick is to use a monotonic increasing stack 
+        result = [0]*len(T) 
+        stack = [] 
         for i, temp in enumerate(T): 
-            while len(stack) > 0 and T[stack[-1]] < temp: 
-                j = stack.pop()
-                wait[j] = i-j
-            stack.append(i)
-        return wait
+            while stack and temp > T[stack[-1]]: 
+                result[stack[-1]] = i - stack[-1]  
+                stack.pop() 
+            stack.append(i) 
+        return result
